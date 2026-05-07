90dd855b772db607889beecd2c5782f89d443dcd
Alberto <garcia.maalb25@triana.salesianos.edu>
2026-05-07 08:38:58 +0200

    feat(modelo): add entidades Mudanza, Operario, Vehiculo, MudanzaVehiculo y enum EstadoMudanza
    Relaciones ManyToMany entre Mudanza y Operario
    Relacion OneToMany entre Mudanza y MudanzaVehiculo
    Enum EstadoMudanza con estados EN_ORIGEN, EN_RUTA, EN_DESTINO, TERMINADO

--- src/main/java/com/salesianostriana/dam/movingprobeta/MudanzaVehiculo.java
+++ src/main/java/com/salesianostriana/dam/movingprobeta/MudanzaVehiculo.java
@@ -1,11 +1,14 @@
 package com.salesianostriana.dam.movingprobeta;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,5 +23,15 @@ public class MudanzaVehiculo {
 	private Long idMudanzaVehiculo;
 	private LocalDateTime fechaAsignacion;
 	private LocalDateTime fechaLiberacion;
+	private String operaciones;
+	@ManyToOne
+	@JoinColumn(name = "mudanza_id")
+	private Mudanza mudanza;
 	
-}
+	@ManyToOne
+	@JoinColumn(name = "vehiculo_id")
+	private Mudanza vehiculo;
+	
+	@Enumerated(EnumType.STRING)
+	private EstadoMudanza estado;
+}                      
