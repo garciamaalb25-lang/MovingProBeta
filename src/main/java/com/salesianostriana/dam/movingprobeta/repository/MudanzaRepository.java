514a0df3e80f59c5c09208cbdd8a4b20c3515277
Alberto <garcia.maalb25@triana.salesianos.edu>
2026-05-07 09:03:25 +0200

    feat(repositorios): add repositories para todas las entidades
    Creación de todos los respository

---
+++ src/main/java/com/salesianostriana/dam/movingprobeta/repository/MudanzaRepository.java
@@ -0,0 +1,8 @@
+package com.salesianostriana.dam.movingprobeta.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.salesianostriana.dam.movingprobeta.Mudanza;
+
+public interface MudanzaRepository extends JpaRepository<Mudanza,Long>{
+
+}
